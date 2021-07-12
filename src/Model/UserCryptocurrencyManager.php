4529369be03473d868520c854f7a34e7d4bc0627
Ali Noureddine <noureddineali900@gmail.com>
2021-07-12 22:34:17 +0300

    Task 1 Modified
    Buy Crypto Modified

--- src/Model/UserCryptocurrencyManager.php
+++ src/Model/UserCryptocurrencyManager.php
@@ -25,13 +25,82 @@ class UserCryptocurrencyManager
     public function addCryptocurrencyToUser(int $userId, Cryptocurrency $cryptocurrency, int $amount): void
     {
         // TODO
-    //    print_r([$userId,$cryptocurrency,$amount]);die();
-        $query= $this->database->prepare('INSERT INTO user_cryptocurrencies values(:userId,:cryptocurrencyId,:amount)');
-        $query->bindParam(':userId', $userId, Database::PARAM_INT);
-        $query->bindParam(':cryptocurrencyId', $cryptocurrency->getId(), Database::PARAM_STR);
-        $query->bindParam(':amount', $amount, Database::PARAM_INT);
+  
+       $cryptocurrencyId=$cryptocurrency->getId();
+        $getData= $this->database->prepare('SELECT c.id, c.name,c.price,u.login,u.funds,uc.amount FROM user_cryptocurrencies AS uc JOIN cryptocurrencies AS c ON uc.cryptocurrency_id = c.id JOIN users AS u ON uc.user_id= u.id WHERE uc.user_id = :user_id');
+        $getData->bindParam(':user_id', $userId, Database::PARAM_INT); 
+        $getData->execute();
+        $result=$getData->fetchAll();
+        
+      
+      $getPrices=$this->database->prepare('SELECT name,price FROM cryptocurrencies Where id= :cryptocurrencyId ');
+      $getPrices->bindParam(':cryptocurrencyId', $cryptocurrencyId, Database::PARAM_STR);
+      $getPrices->execute();  
+      $CoinPrice=$getPrices->fetchAll();
 
-        $query->execute();
+        if($amount > 0){
+            foreach ($result as $r) {
+                # code...
+                $totalPrice=$CoinPrice[0][1]*$amount;
+                $funds=$r['funds'];
+               
+            }
+            
+            if ($totalPrice>$funds) {
+                print_r("Insufficent Funds");die();
+            } else {
+                # code...
+                $CheckRecord=$this->database->prepare('SELECT cryptocurrency_id,amount FROM user_cryptocurrencies LEFT JOIN users ON users.id=user_cryptocurrencies.user_id Where cryptocurrency_id= :cryptocurrencyId AND user_id = :userId ');
+                $CheckRecord->bindParam(':userId', $userId, Database::PARAM_INT);
+                $CheckRecord->bindParam(':cryptocurrencyId', $cryptocurrencyId, Database::PARAM_STR);
+                $CheckRecord->execute();  
+                $check=$CheckRecord->fetchAll();
+                
+               
+                $count=count($check);
+                if($count==0){
+                    $query= $this->database->prepare('INSERT INTO user_cryptocurrencies values(:userId,:cryptocurrencyId,:amount)');
+                    $query->bindParam(':userId', $userId, Database::PARAM_INT);
+                    $query->bindParam(':cryptocurrencyId', $cryptocurrencyId, Database::PARAM_STR);
+                    $query->bindParam(':amount', $amount, Database::PARAM_INT);
+            
+                    $query->execute();
+                    $remainFunds=$funds-$totalPrice;
+                   
+                    $sql = $this->database->prepare(' UPDATE users SET funds=:funds WHERE id=:userId' );
+                    $sql->bindParam(':userId', $userId, Database::PARAM_INT);
+                    $sql->bindParam(':funds', $remainFunds, Database::PARAM_INT);
+                    $sql->execute();
+                    
+                   
+                } else {
+                    # code...
+               
+                    $remainFunds=$funds-$totalPrice;
+                    $UpdateAmount=$check[0][1]+$amount;
+           
+                    $sql = $this->database->prepare(' UPDATE users,user_cryptocurrencies SET funds=:funds , amount=:amount  WHERE id=:userId AND user_cryptocurrencies.cryptocurrency_id=:cryptocurrencyId'  );
+                    $sql->bindParam(':funds', $remainFunds, Database::PARAM_INT);
+                    $sql->bindParam(':amount', $UpdateAmount, Database::PARAM_INT);
+                    $sql->bindParam(':userId', $userId, Database::PARAM_INT);
+                    $sql->bindParam(':cryptocurrencyId', $cryptocurrencyId, Database::PARAM_STR);
+
+                    
+                    $sql->execute();
+
+                }
+            }
+            
+          
+        
+        }
+        else
+            {
+                print_r("Amount Should be Greater than 0");die();
+            }
+        
+        
+      
     }
 
     public function subtractCryptocurrencyFromUser(int $userId, Cryptocurrency $cryptocurrency, int $amount): void
